cd532de929988fa33bc8c0d87664dda666855c12
Erickxse <erick_sebastian_@hotmail.com>
2022-07-13 12:15:00 -0500

    constructorcliente

--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -10,5 +10,26 @@ public class Cliente {
     private String direccion;
     private String nCedula;
     private String nCelular;
+
+    public Cliente(String nombre, String apellido, String usuario, String clave, 
+            String ciudad, String direccion, String nCedula, String nCelular) {
+        
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.usuario = usuario;
+        this.clave = clave;
+        this.ciudad = ciudad;
+        this.direccion = direccion;
+        this.nCedula = nCedula;
+        this.nCelular = nCelular;
+    }
+
+    public Cliente() {
+    }
+    
+    
+    
+    
+    
     
 }
