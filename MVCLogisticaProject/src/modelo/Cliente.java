0ab8130c6a36a061896aef8a3f0f515023100afe
Erickxse <erick_sebastian_@hotmail.com>
2022-07-12 20:26:27 -0500

    mvcclases1

---
+++ MVCLogisticaProject/src/modelo/Cliente.java
@@ -0,0 +1,14 @@
+package modelo;
+
+public class Cliente {
+    
+    private String nombre;
+    private String apellido;
+    private String usuario;
+    private String clave;
+    private String ciudad;
+    private String direccion;
+    private String nCedula;
+    private String nCelular;
+    
+}
---
+++ MVCLogisticaProject/src/modelo/Paquete.java
@@ -0,0 +1,10 @@
+package modelo;
+
+public class Paquete {
+    
+    private String codigo;
+    private Ciudad ciudad1;
+    private Ciudad ciudad2;
+    private String direccion1;
+    private String direccion2;
+}
