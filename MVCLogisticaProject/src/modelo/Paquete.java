d88dffd7515723823769c59bbdd2e0c9dd992aa2
Erickxse <erick_sebastian_@hotmail.com>
2022-07-12 20:41:04 -0500

    mvcclases2

--- MVCLogisticaProject/src/modelo/Paquete.java
+++ MVCLogisticaProject/src/modelo/Paquete.java
@@ -3,6 +3,7 @@ package modelo;
 public class Paquete {
     
     private String codigo;
+    private double peso;
     private Ciudad ciudad1;
     private Ciudad ciudad2;
     private String direccion1;
